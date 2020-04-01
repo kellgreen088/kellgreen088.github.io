38fc97988d0800e9b6e50f717cdb4d999f6aa71d
kellgreen088 <56590317+kellgreen088@users.noreply.github.com>
2020-04-01 21:59:05 +1000

    Add contact form php file

--- process_form.php
+++ process_form.php
@@ -11,13 +11,13 @@ $mail_body = "Name : ".$name."<br>";
 $mail_body .= "Email : " . $email . "<br>";
 $mail_body .= "Subject : " . $subject . "<br>";
 $mail_body .= "Message : " . $message . "<br>";
-if(mail("youremail@mail.com","From: Contact Form Your name",$mail_body,$headers)){
-    $thanks_mail_body = "Hello " . $name ."<br>";
+if(mail("kelly@akikoartdesign.com.au","From: Kelly at Akiko Art & Design",$mail_body,$headers)){
+    $thanks_mail_body = "Hello" . $name ."<br>";
     
-    $thanks_mail_body .= "Hey! Thanks for your email!<br>";
+    $thanks_mail_body .= "Thanks for your message!<br>";
     $thanks_mail_body .= "I will get back to you soon.<br>";
-    $thanks_mail_body .= "Thank You,<br>";
-    $thanks_mail_body .= "Your Name.";
+    $thanks_mail_body .= "Cheers,<br>";
+    $thanks_mail_body .= "Kelly";
 
     if(mail($email,"Contact Request Notification Email",$thanks_mail_body,$headers)){
         echo "1";
